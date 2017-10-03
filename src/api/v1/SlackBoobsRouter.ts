acb4fb62e6109416c8f0709e27f9086d385ef65d
serioga-ninja <sergeidatsiy@gmail.com>
2017-10-03 18:01:14 +0300

    set the right regular expression for the thumbnails

--- src/api/v1/SlackBoobsRouter.ts
+++ src/api/v1/SlackBoobsRouter.ts
@@ -3,14 +3,25 @@ import {ISlackEventRequestBody} from '../../interfaces/i-slack-event-request-bod
 import {RouterClass} from '../../classes/router.class';
 import * as request from 'request';
 
+const thumbnailReg = new RegExp(/"thumbnail_src": "([\w:\/\-\.\n]+)/g);
+
+function getMatches(string, regex, index): string[] {
+    index || (index = 1); // default to the first capturing group
+    let matches = [];
+    let match;
+    while (match = regex.exec(string)) {
+        matches.push(match[index]);
+    }
+    return matches;
+}
+
 export class SlackBoobsRouter extends RouterClass {
 
     public getSomeBoobs(req: Request, res: Response, next: NextFunction) {
         let body: ISlackEventRequestBody = req.body;
 
         request.get('https://www.instagram.com/explore/tags/cats/', (err, result) => {
-            console.log((<any>result).body);
-            let results: string[] = (<any>result).body.match(/(https:\/\/instagram.fiev3-1.fna.fbcdn.net\/[\w\.-]+\/[\w\.-]+\/\w+\.jpg)/g);
+            let results: string[] = getMatches((<any>result).body, thumbnailReg, 1);
 
             let data = {
                 response_type: 'in_channel', // public to the channel
