b00be7e911a46eb3b308b395c28977a4d8d03d07
serioga-ninja <sergeidatsiy@gmail.com>
2017-10-03 16:23:28 +0300

    allow to parse images from the instagram by tagname

--- src/api/v1/SlackBoobsRouter.ts
+++ src/api/v1/SlackBoobsRouter.ts
@@ -1,23 +1,28 @@
 import {Request, Response, NextFunction} from 'express';
 import {ISlackEventRequestBody} from '../../interfaces/i-slack-event-request-body';
 import {RouterClass} from '../../classes/router.class';
+import * as request from 'request';
 
 export class SlackBoobsRouter extends RouterClass {
 
     public getSomeBoobs(req: Request, res: Response, next: NextFunction) {
         let body: ISlackEventRequestBody = req.body;
 
-        let data = {
-            response_type: 'in_channel', // public to the channel
-            text: '302: Found',
-            attachments: [{
-                image_url: 'https://http.cat/302.jpg'
-            }]
-        };
+        request.get('https://www.instagram.com/explore/tags/cats/', (err, result) => {
+            let results: string[] = (<any>result).body.match(/(https:\/\/instagram.fiev3-1.fna.fbcdn.net\/[\w\.-]+\/[\w\.-]+\/\w+\.jpg)/g);
 
-        // TODO:  return BOOOBS!
+            let data = {
+                response_type: 'in_channel', // public to the channel
+                text: '',
+                attachments: [{
+                    image_url: results[Math.floor(Math.random() * results.length)]
+                }]
+            };
 
-        res.json(data);
+            // TODO:  return BOOOBS!
+
+            res.json(data);
+        });
     }
 
     /**
