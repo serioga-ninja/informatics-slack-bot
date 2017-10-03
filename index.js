7225ace2483ac14794d64da920ff68de69892fd3
serioga-ninja <sergeidatsiy@gmail.com>
2017-10-03 17:20:21 +0300

    compile before start

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-require('ts-node').register({ /* options */ });
-require('dotenv').load();
-
-require('./src/index');
+if (process.env.NODE_ENV === 'development') {
+    require('ts-node').register({/* options */});
+    require('dotenv').load();
+    require('./src/index');
+} else if (process.env.NODE_ENV === 'production') {
+    require('./build/index');
+}
