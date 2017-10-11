f3fcd9dd44059427b22f9e84d4581c9a194e9da7
serioga-ninja <sergeidatsiy@gmail.com>
2017-10-11 14:16:29 +0300

    allow .env in the prod mode

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ if (process.env.NODE_ENV === 'development') {
     require('dotenv').load();
     require('./src/index');
 } else if (process.env.NODE_ENV === 'production') {
+    try {
+        require('dotenv').load();
+    } catch (e) {
+    }
+
     require('./build/index');
 }
 
