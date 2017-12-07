c40323de0ace25adfcfbb8fe5a7280c9df86faf9
serioga-ninja <sergeidatsiy@gmail.com>
2017-12-07 13:33:34 +0200

    sent redirect_url on registration

--- src/modules/slack-apps/commands/registration.command.ts
+++ src/modules/slack-apps/commands/registration.command.ts
@@ -1,3 +1,4 @@
+import * as qs from 'querystring';
 import {ISlackWebhookRequestBody} from '../../../interfaces/i-slack-webhook-request-body';
 import {BaseCommand} from '../../core/BaseCommand.class';
 import {ChannelNotRegistered} from '../../core/CommandDecorators';
@@ -22,7 +23,12 @@ class RegistrationCommand extends BaseCommand {
             text: '',
             attachments: [
                 {
-                    title_link: `https://slack.com/oauth/authorize?scope=incoming-webhook,commands&client_id=${variables.slack.CLIENT_ID}`,
+                    title_link: `https://slack.com/oauth/authorize?${qs.stringify({
+                        scope: 'incoming-webhook,commands',
+                        client_id: variables.slack.CLIENT_ID,
+                        redirect_uri: `http://${variables.domainUrl}/api/v1/events/oauth-callback`,
+                        team: requestBody.team_id
+                    })}`,
                     title: 'Click to init',
                     image_url: 'https://platform.slack-edge.com/img/add_to_slack.png'
                 }
